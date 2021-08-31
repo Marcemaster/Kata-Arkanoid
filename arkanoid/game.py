c2b812ceaee2243427e2f113438de66dbfc0e1b5
Marcelo Hornillos <marcehornillos@gmail.com>
2021-09-01 00:42:22 +0200

    primera foto

--- arkanoid/game.py
+++ arkanoid/game.py
@@ -1,23 +1,15 @@
 import pygame as pg
+from . import ANCHO, ALTO
+from arkanoid.escenas import Portada
 
 pg.init()
 
 class Game():
     def __init__(self):
-        self.pantalla = pg.display.set_mode((ANCHO,ALTO))
+        pantalla = pg.display.set_mode((ANCHO, ALTO))
+        self.escenas = [Portada(pantalla)]
 
     def start(self):
-        game_over = False
-
-        while not game_over:
-            for evento in pg.event.get():
-                if evento.type == pg.QUIT:
-                    game_over = True
-
-            self.pantalla.fill((123, 123, 255))
-
-            pg.display.flip()
-
+        self.escenas[0].bucle_principal()
         pg.quit()
-
 
